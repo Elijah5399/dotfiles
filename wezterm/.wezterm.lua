81b618a8fbb9b1138774e9b6e3b2c1ad3da9d2e0
Elijah Chia <chia.elijah@gmail.com>
2025-06-30 17:20:50 +0800

    initial commit

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,60 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.keys = {
+	{key = 'v', mods = 'CTRL', action = act.PasteFrom('Clipboard')},
+	{key = 'v', mods = 'CTRL', action = act.PasteFrom('PrimarySelection')} 
+}
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    theme = 'tokyonight',
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = { 'mode' },
+    tabline_b = { 'workspace' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { 'ram', 'cpu' },
+    tabline_y = { 'datetime', 'battery' },
+    tabline_z = { 'domain' },
+  },
+  extensions = {},
+})
+tabline.apply_to_config(config)
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.color_scheme = 'AdventureTime'
+
+-- Finally, return the configuration to wezterm:
+return config
